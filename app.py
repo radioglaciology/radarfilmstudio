9592ab3a5e0334006558c64af23663d7441ef453
Thomas Teisberg <teisberg@stanford.edu>
2020-04-11 15:12:02 -0700

    next/previous buttons and loading improvements

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from threading import Thread
 import os
 from io import BytesIO
 from PIL import Image
+import numpy as np
 
 from flask import Flask, render_template, url_for, request, redirect, Response, send_from_directory, send_file
 from flask_sqlalchemy import SQLAlchemy
@@ -34,6 +35,12 @@ ma = Marshmallow(app)
 api = Api(app)
 images = Images(app)
 
+# To play with the db in a REPL:
+# > from app import db, app
+# > from film_segment import FilmSegment
+# > app.app_context().push()
+# > db.session.query(FilmSegment).filter( ... bla ... ) # or whatever
+
 
 class FilmSegmentSchema(ma.Schema):
     class Meta:
@@ -47,7 +54,43 @@ segment_schema = FilmSegmentSchema()
 class FilmSegmentResource(Resource):
     def get(self, id):
         seg = FilmSegment.query.get_or_404(id)
-        return segment_schema.dump(seg)
+        seg_dict = segment_schema.dump(seg)
+
+        # Add next/prev by frame order
+        prev_frame = min(seg_dict['first_frame'], seg_dict['last_frame']) - 1
+        next_frame = max(seg_dict['first_frame'], seg_dict['last_frame']) + 1
+
+        next_by_frame = FilmSegment.query.filter(
+                (FilmSegment.first_frame >= next_frame) & (FilmSegment.last_frame >= next_frame) & (FilmSegment.reel == seg.reel)
+            ).order_by(FilmSegment.first_frame.asc()).first()
+        prev_by_frame = FilmSegment.query.filter(
+                (FilmSegment.first_frame <= prev_frame) & (FilmSegment.last_frame <= prev_frame) & (FilmSegment.reel == seg.reel)
+            ).order_by(FilmSegment.first_frame.desc()).first()
+
+        if prev_by_frame:
+            seg_dict['prev_by_frame'] = prev_by_frame.id
+        if next_by_frame:
+            seg_dict['next_by_frame'] = next_by_frame.id
+
+        # Add next/prev by CBD order
+        prev_cbd = min(seg_dict['first_cbd'], seg_dict['last_cbd']) - 1
+        next_cbd = max(seg_dict['first_cbd'], seg_dict['last_cbd']) + 1
+
+        next_by_cbd = FilmSegment.query.filter(
+            (FilmSegment.first_cbd >= next_cbd) & (FilmSegment.last_cbd >= next_cbd) & (
+                        FilmSegment.flight == seg.flight)
+        ).order_by(FilmSegment.first_cbd.asc()).first()
+        prev_by_cbd = FilmSegment.query.filter(
+            (FilmSegment.first_cbd <= prev_cbd) & (FilmSegment.last_cbd <= prev_cbd) & (
+                        FilmSegment.flight == seg.flight)
+        ).order_by(FilmSegment.first_cbd.desc()).first()
+
+        if prev_by_cbd:
+            seg_dict['prev_by_cbd'] = prev_by_cbd.id
+        if next_by_cbd:
+            seg_dict['next_by_cbd'] = next_by_cbd.id
+
+        return seg_dict
 
     def post(self, id):
         seg = FilmSegment.query.get_or_404(id)
@@ -107,7 +150,6 @@ def flight_page(flight_id):
 @app.route('/update_form/<int:id>/')
 def update_page(id):
     seg = FilmSegment.query.get(id)
-    print(seg.is_verified)
     return render_template("update.html", segment=seg, breadcrumbs=[('Explorer', '/')])
 
 def serve_pil_image(pil_img):
@@ -117,6 +159,7 @@ def serve_pil_image(pil_img):
     return send_file(img_io, mimetype='image/jpeg')
 
 @app.route('/api/radargram/jpg/<int:id>')
+@app.route('/api/radargram/jpg/<int:id>.jpg')
 @app.route('/api/radargram/jpg/<int:id>/h/<int:max_height>')
 def radargram_jpg(id, max_height = None):
     base_path = "/data3/schroeder/jemmons/35mm_output.2018-01-12"
@@ -136,6 +179,14 @@ def radargram_jpg(id, max_height = None):
     else:
         return send_from_directory(base_path,
                                     filename)
+
+@app.route('/api/radargram/tiff/<int:id>')
+@app.route('/api/radargram/tiff/<int:id>.tiff')
+def radargram_tiff(id):
+    base_path = "/data3/schroeder/jemmons/35mm_output.2018-01-12"
+    seg = FilmSegment.query.get(id)
+    return send_from_directory(base_path, seg.path)
+
 
 
 # def bk_worker():
